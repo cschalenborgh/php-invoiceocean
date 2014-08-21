3109926bf085879e53b7ddd7c3d706175402cb56
Chris Schalenborgh <chris.s@kryap.com>
2014-08-21 18:50:01 +0200

    more samples

---
+++ example/add-client.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * This example file will add a client
+ */
+
+$io = new InvoiceOceanClient('username', 'api_token_goes_here');
+
+    $client = array(
+        'name'          => 'Chris Schalenborgh',
+        'tax_no'        => 1,
+        'bank'          => 'My Bank',
+        'bank_account'  => '001-123456-78',
+        'city'          => 'Maasmechelen',
+        'country'       => 'BE',
+        'email'         => 'chris@schalenborgh.be',
+        'person'        => '',
+        'post_code'     => '1234',
+        'phone'         => '+32.484680765',
+        'street'        => 'Street',
+        'street_no'     => '123'
+    );
+
+$result = $io->addClient($client);
+
+var_dump($result);
+
+?>
--- example/get-clients.php
+++ example/get-clients.php
@@ -1,6 +1,10 @@
 <?php
+/*
+ * This example file will return your clients
+ */
 
 $io = new InvoiceOceanClient('username', 'api_token_goes_here');
+
 $clients = $io->getClients();
 
 var_dump($clients);
